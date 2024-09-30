c9e32ced79d51b8c38f9372c2270ab6f01ab5fa4
Andrew Marfo <andrewmarfo23@gmail.com>
2024-09-30 19:27:49 +0000

    background image

--- src/App.js
+++ src/App.js
@@ -32,10 +32,14 @@ const App = () => {
   
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => {
+      {categories.map(({ title, id, imageUrl }) => {
         return (
           <div className="category-container" key={id}>
-            <div className='background-image'div/>
+            <div className='background-image' div
+              style={{
+                backgroundImage: `url(${imageUrl})`,
+              }}
+            />
             <div className="category-body-container">
               <h2>{title}</h2>
               <p>Shop Now</p>
