c5d4ec3d7f0ab253aa48a7a25a7b232e2a8de142
Andrew Marfo <andrewmarfo23@gmail.com>
2024-09-30 18:56:27 +0000

    sass and styling

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
+import './categories.styles.scss'
+
 const App = () => {
 
   const categories = [
     {
-      'id': 1,
-      'title': 'Hats',
+      id: 1,
+      title: 'Hats',
     },
     {
-      'id': 2,
-      'title':'Sneakers'
+      id: 2,
+      title:'Sneakers'
     },
     {
-      'id': 3,
-      'title':'Jacket'
+      id: 3,
+      title:'Jacket'
     },
     {
-      'id': 4,
-      'title':'Men'
+      id: 4,
+      title:'Womens'
     },
     {
-      'id': 5,
-      'title':'Women'
+      id: 5,
+      title:'Mens'
     }
   ]
   return (
-    <div className="categories-conatianer">
+    <div className="categories-container">
       {categories.map(({ title, id }) => {
         return (
           <div className="category-container" key={id}>
+            <div className='background-image'div/>
             <div className="category-body-container">
               <h2>{title}</h2>
               <p>Shop Now</p>
