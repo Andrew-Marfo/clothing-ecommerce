6e66e98870df55a8b1986e746c66cc78bde8f491
Andrew Marfo <andrewmarfo23@gmail.com>
2024-10-06 12:36:40 +0000

    routing navigation, styling

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import './navigation.styles.scss';
 import { ReactComponent as Crownlogo } from '../../assets/crown.svg';
 
 const NavigationBar = () => {
     return (
         <Fragment>
-            <div>
+            <div className={"navigation"}>
                 <Link className="logo-container" to={'/'}>
                     <Crownlogo className={'logo'} />
                 </Link>
