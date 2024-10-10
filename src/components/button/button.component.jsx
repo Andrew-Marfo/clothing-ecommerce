0821d20267257e1db40b4ad9f6710b7ead48fc05
Andrew Marfo <andrewmarfo23@gmail.com>
2024-10-10 23:15:12 +0000

    custom button container

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,14 @@
+import './button.styles.scss';
+
+const ButtonType = {
+    google: "google-sign-in",
+    inverted: "inverted",
+}
+
+const Button = ({children, buttonType, ...otherProps }) => {
+    return (
+        <button className={`${ButtonType[buttonType]} button-container`} {...otherProps}>
+            {children}
+        </button>
+    );
+}
